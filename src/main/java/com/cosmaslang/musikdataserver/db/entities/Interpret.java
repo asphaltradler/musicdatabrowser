d7ad6affbe2e8b734cbdda60d079558448caf9b6
Cosmas Lang <cosmas.lang@deutschebahn.com>
2024-10-11 17:57:49 +0200

    Unendliche Rekursion bei Interpret verhindern.

--- src/main/java/com/cosmaslang/musikdataserver/db/entities/Interpret.java
+++ src/main/java/com/cosmaslang/musikdataserver/db/entities/Interpret.java
@@ -1,5 +1,6 @@
 package com.cosmaslang.musikdataserver.db.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
@@ -14,7 +15,8 @@ public class Interpret extends NamedEntity {
     //muss man leider hier drin definieren, sonst wird es nicht gefunden
     private String name;
 
-    @ManyToMany(mappedBy = "interpreten", fetch = FetchType.EAGER)
+    @ManyToMany(mappedBy = "interpreten", fetch = FetchType.LAZY)
+    @JsonBackReference
     private Set<Track> tracks = new HashSet<>();
 
     @Override
