9a3908ed0cea04f01aa586347080e92a3edab784
Cosmas Lang <cosmas.lang@deutschebahn.com>
2024-10-15 17:32:46 +0200

    Weitere Methoden für Suche, diesmal nicht über angepasste Queries sondern in Java

--- src/main/java/com/cosmaslang/musikdataserver/controller/GenreRestController.java
+++ src/main/java/com/cosmaslang/musikdataserver/controller/GenreRestController.java
@@ -1,10 +1,12 @@
 package com.cosmaslang.musikdataserver.controller;
 
 import com.cosmaslang.musikdataserver.db.entities.Genre;
+import com.cosmaslang.musikdataserver.db.entities.Track;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController
@@ -25,7 +27,10 @@ public class GenreRestController extends AbstractMusikRestController<Genre> {
         if (id != null) {
             return getEntitiesIfExists(id, genreRepository);
         } else if (genre != null) {
-            return genreRepository.findByNameContaining(genre);
+            return genreRepository.findByNameContainingIgnoreCase(genre);
+        } else if (komponist != null) {
+            List<Track> tracks = trackRepository.findByKomponist(komponist);
+            return tracks.stream().map(Track::getGenres).flatMap(Collection::stream).distinct().toList();
         }
         return getAll(genreRepository);
     }
--- src/main/java/com/cosmaslang/musikdataserver/controller/KomponistRestController.java
+++ src/main/java/com/cosmaslang/musikdataserver/controller/KomponistRestController.java
@@ -1,11 +1,13 @@
 package com.cosmaslang.musikdataserver.controller;
 
 import com.cosmaslang.musikdataserver.db.entities.Komponist;
+import com.cosmaslang.musikdataserver.db.entities.Track;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/musik/komponist")
@@ -25,7 +27,18 @@ public class KomponistRestController extends AbstractMusikRestController<Komponi
         if (id != null) {
             return getEntitiesIfExists(id, komponistRepository);
         } else if (komponist != null) {
-            return komponistRepository.findByNameContaining(komponist);
+            return komponistRepository.findByNameContainingIgnoreCase(komponist);
+        } else if (album != null) {
+            //Hier können mehrere Komponisten erscheinen, da die Zuordnung track-weise ist.
+            //Außerdem können durch "like" ja mehrere Alben gefunden werden.
+            //Das ganze könnte man alternativ auch wie in AlbumRepository/Controller über
+            //eigene Queries mit JOIN machen
+            List<Track> tracks = trackRepository.findByAlbumLike(album);
+            Stream<Komponist> komponistStream = tracks.stream().map(Track::getKomponist);
+            return komponistStream.distinct().toList();
+        } else if (genre != null) {
+            List<Track> tracks = trackRepository.findByGenreLike(genre);
+            return tracks.stream().map(Track::getKomponist).distinct().toList();
         }
         return getAll(komponistRepository);
     }
