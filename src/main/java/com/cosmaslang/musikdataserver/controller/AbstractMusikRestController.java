3f360130ac4d62425d5c781537697b38e54b0c67
Cosmas Lang <cosmas.lang@deutschebahn.com>
2024-10-24 12:10:14 +0200

    CORS nötig für Angular Client

--- src/main/java/com/cosmaslang/musikdataserver/controller/AbstractMusikRestController.java
+++ src/main/java/com/cosmaslang/musikdataserver/controller/AbstractMusikRestController.java
@@ -17,6 +17,7 @@ import java.util.*;
 import java.util.logging.Logger;
 
 @NoRepositoryBean
+@CrossOrigin(origins = "http://localhost:4200")
 public abstract class AbstractMusikRestController<ENTITY extends NamedEntity> {
     protected Logger logger = Logger.getLogger(this.getClass().getName());
 
