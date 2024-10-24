ea0ef5dc933a635b9d61fb5301620fe7a4219f3d
Cosmas Lang <cosmas.lang@deutschebahn.com>
2024-10-24 17:26:58 +0200

    CORS nötig für Angular Client

--- src/main/java/com/cosmaslang/musikdataserver/controller/HelloController.java
+++ src/main/java/com/cosmaslang/musikdataserver/controller/HelloController.java
@@ -4,11 +4,13 @@ import com.cosmaslang.musikdataserver.db.entities.*;
 import com.cosmaslang.musikdataserver.db.repositories.NamedEntityRepository;
 import com.cosmaslang.musikdataserver.db.repositories.TrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping({"/", "/musik"})
 public class HelloController {
     @Autowired
